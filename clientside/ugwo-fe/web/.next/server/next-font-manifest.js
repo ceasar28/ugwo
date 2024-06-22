01cc5b752a49b706a8700629e089269897aa875a
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-06-22 22:50:18 +0100

    integration

---
+++ clientside/ugwo-fe/web/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/user/Dev/Projects/ugwo/clientside/ugwo-fe/web/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\",\"static/media/92f44bb82993d879-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
