6859cf3a479cc31fea956c32e686a6a2ee63ab91
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-06-23 16:13:21 +0000

    update

--- clientside/src/pages/dashboard/Dashboard.tsx
+++ clientside/src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,15 @@
 import Top from './screen/Top'
+import Transaction from './screen/Transaction'
 
 
 
 const Dashboard = () => {
     return (
-        <section className='md:pl-[16rem] bg-[--text-extra] min-h-[30rem] top-0 overflow-x-hidden overflow-y-auto'>
+        <section className='md:pl-[14rem] bg-[--text-extra] min-h-[30rem] top-0 overflow-x-hidden overflow-y-auto'>
             <div className="grid justify-between">
                <div className="max-w-[120rem] grid justify-between py-10 gap-6 mx-4">
                     <Top/>
+                    <Transaction/>
                 </div>
             </div>            
         </section>
