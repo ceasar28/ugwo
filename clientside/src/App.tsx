0835d47966da0f0431b4be972be47b6b7b2b8fa1
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-06-23 04:33:17 +0100

    integration

--- clientside/src/App.tsx
+++ clientside/src/App.tsx
@@ -1,37 +1,35 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import MainContent from './pages/main/MainContent';
-import Transaction from './pages/Transaction/Transaction';
-import Send from './pages/action/Send';
-import Request from './pages/action/Request';
-import Invites from './pages/invitess/Invites';
-import Deposit from './pages/action/Deposit';
-import Sidebar from './layout/Sidebar';
-import Connect from './pages/connect/Connect';
-import NavbarWrapper from './context/NavbarWrapper';
-import FloatingNavbar from './layout/FloatingNavbar';
-import Dashboard from './pages/dashboard/Dashboard';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import MainContent from "./pages/main/MainContent";
+import Transaction from "./pages/Transaction/Transaction";
+import Send from "./pages/action/Send";
+import Request from "./pages/action/Request";
+import Invites from "./pages/invitess/Invites";
+import Deposit from "./pages/action/Deposit";
+import Sidebar from "./layout/Sidebar";
+import Connect from "./pages/connect/Connect";
+import NavbarWrapper from "./context/NavbarWrapper";
+import FloatingNavbar from "./layout/FloatingNavbar";
+import Dashboard from "./pages/dashboard/Dashboard";
 
-
-const App  = () => {
+const App = () => {
   return (
     <Router>
       <NavbarWrapper>
-      <Sidebar/>
-      <FloatingNavbar/>
-      </NavbarWrapper>      
+        <Sidebar />
+        <FloatingNavbar />
+      </NavbarWrapper>
       <Routes>
-        <Route index element={<MainContent/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/connect' element={<Connect/>}/>
-        <Route path='/transaction' element={<Transaction/>}/>
-        <Route path='/send' element={<Send/>}/>
-        <Route path='/request' element={<Request/>}/>
-        <Route path='/invite' element={<Invites/>}/>
-        <Route path='/deposit' element={<Deposit/>}/> 
-           
+        <Route index element={<MainContent />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/connect" element={<Connect />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/send" element={<Send />} />
+        <Route path="/request" element={<Request />} />
+        <Route path="/invite" element={<Invites />} />
+        <Route path="/deposit" element={<Deposit />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
