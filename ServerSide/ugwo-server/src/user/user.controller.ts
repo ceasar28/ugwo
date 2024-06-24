8d4b72ca6bc1330d9c547d1cc1c42df59bc60990
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-06-25 00:58:56 +0100

    server

--- ServerSide/ugwo-server/src/user/user.controller.ts
+++ ServerSide/ugwo-server/src/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: Prisma.UserCreateInput) {
+    return await this.userService.createUser(createUserDto);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: Prisma.UserUpdateInput,
+  ) {
+    return await this.userService.updateUser(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.userService.delete(+id);
+  }
+
+  @Get(':displayName')
+  async findUserAddress(@Param('displayName') displayName: string) {
+    return await this.userService.findUserAddress(displayName);
+  }
+
+  @Get('users')
+  async findAllUser() {
+    return await this.userService.findAllUser();
+  }
+}
--- ServerSide/ugwo-server/src/user/user.service.ts
+++ ServerSide/ugwo-server/src/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async createUser(createUserDto: Prisma.UserCreateInput) {
+    return await this.databaseService.user.create({ data: createUserDto });
+  }
+
+  async updateUser(id: number, updateUserDto: Prisma.UserUpdateInput) {
+    return await this.databaseService.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  async delete(id: number) {
+    return await this.databaseService.user.delete({ where: { id } });
+  }
+
+  async findAllUser() {
+    return await this.databaseService.user.findMany();
+  }
+  async findUserAddress(displayName: string) {
+    const user = await this.databaseService.user.findFirst({
+      where: { displayName },
+    });
+    return { address: user.walletAddress };
+  }
+}
