24e1a361316608f85c728e92ced64262e79b84a3
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-07-01 05:08:21 +0100

    linting

--- clientside/src/utils/wagmi.ts
+++ clientside/src/utils/wagmi.ts
@@ -1,5 +1,5 @@
 import { http, createConfig } from "wagmi";
-import { baseSepolia, klaytnBaobab, sepolia } from "wagmi/chains";
+import { base, baseSepolia, klaytnBaobab, sepolia } from "wagmi/chains";
 import { coinbaseWallet } from "wagmi/connectors";
 
 export const config = createConfig({
